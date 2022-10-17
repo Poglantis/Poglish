aee18bb8a2a9729e7111ccc0284d5b03a1e18c86
Pogging <dylstructor@gmail.com>
2022-10-17 17:31:03 -0500

    e

---
+++ generator.py
@@ -0,0 +1,12 @@
+starts = ["∁α","αx","⤐","⫝","α⇂","ȡ","ȅ"]
+middles = ["lα","τlo","☼","αlα","loβ","ƿ"]
+ends = ["βlαβ","loβ","⭉","τlγ","τlo","l϶","ϵf"]
+import random
+
+words_to_be_genned = []
+def new_word():
+    return random.choice(starts)+random.choice(middles)+random.choice(ends)
+with open("New_Words.txt","w") as t:
+    for i in words_to_be_genned:
+        t.write("{} -> ".format(i))
+        t.write(new_word()+"\n")
