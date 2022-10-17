27be7dcd3ab31c2ff38c509eb91ea984fa7bd310
Pogging <dylstructor@gmail.com>
2022-10-17 17:49:53 -0500

    New words

--- generator.py
+++ generator.py
@@ -3,9 +3,17 @@ middles = ["lα","τlo","☼","αlα","loβ","ƿ"]
 ends = ["βlαβ","loβ","⭉","τlγ","τlo","l϶","ϵf"]
 import random
 
-words_to_be_genned = []
+words_to_be_genned = ["Bad"]
 def new_word():
-    return random.choice(starts)+random.choice(middles)+random.choice(ends)
+    i = random.choice([0,0,0,0,1,1,1,2,2,2,3])
+    if i == 0:
+        return random.choice(starts)+random.choice(middles)+random.choice(ends)
+    elif i == 1:
+        return random.choice(starts)+random.choice(starts)+random.choice(middles)+random.choice(ends)
+    elif i == 2:
+        return random.choice(starts)+random.choice(starts)+random.choice(middles)+random.choice(middles)+random.choice(ends)
+    else:
+        return random.choice(starts)+random.choice(starts)+random.choice(middles)+random.choice(middles)+random.choice(ends)+random.choice(ends)
 with open("New_Words.txt","w") as t:
     for i in words_to_be_genned:
         t.write("{} -> ".format(i))
