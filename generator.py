697805c2675c821fa9fa238f9f06d8d5a57de193
Pogging <dylstructor@gmail.com>
2022-10-17 17:35:37 -0500

    Coffee

--- generator.py
+++ generator.py
@@ -3,7 +3,7 @@ middles = ["lα","τlo","☼","αlα","loβ","ƿ"]
 ends = ["βlαβ","loβ","⭉","τlγ","τlo","l϶","ϵf"]
 import random
 
-words_to_be_genned = ["Runs"]
+words_to_be_genned = []
 def new_word():
     return random.choice(starts)+random.choice(middles)+random.choice(ends)
 with open("New_Words.txt","w") as t:
